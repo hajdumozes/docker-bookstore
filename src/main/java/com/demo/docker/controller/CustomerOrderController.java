14193bcd54598eb8b8a398930daa6a81c85d475a
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-07-27 21:46:26 +0200

    list endpoint

--- src/main/java/com/demo/docker/controller/CustomerOrderController.java
+++ src/main/java/com/demo/docker/controller/CustomerOrderController.java
@@ -1,11 +1,15 @@
 package com.demo.docker.controller;
 
+import com.demo.docker.entity.Book;
+import com.demo.docker.entity.CustomerOrder;
 import com.demo.docker.service.CustomerOrderService;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/order")
@@ -28,6 +33,21 @@ class CustomerOrderController {
         return ResponseEntity.ok().body(orderId);
     }
 
+    @GetMapping(value = "/list/{customerId}")
+    ResponseEntity<List<CustomerOrderResponse>> list(@PathVariable("customerId") long customerId) {
+        List<CustomerOrder> orders = customerOrderService.findAllBy(customerId);
+        List<CustomerOrderResponse> response = orders.stream().map(this::of).collect(Collectors.toList());
+        return ResponseEntity.ok().body(response);
+    }
+
+    private CustomerOrderResponse of(CustomerOrder customerOrder) {
+        CustomerOrderResponse customerOrderResponse = new CustomerOrderResponse();
+        customerOrderResponse.setOrderId(customerOrder.getId());
+        List<Long> bookIds = customerOrder.getBooks().stream().map(Book::getId).collect(Collectors.toList());
+        customerOrderResponse.setBookIds(bookIds);
+        return customerOrderResponse;
+    }
+
     @Data
     @FieldDefaults(level = AccessLevel.PRIVATE)
     public static class CreateOrderRequest {
@@ -35,6 +55,13 @@ class CustomerOrderController {
         long customerId;
 
         @NotNull
+        List<Long> bookIds;
+    }
+
+    @Data
+    @FieldDefaults(level = AccessLevel.PRIVATE)
+    public static class CustomerOrderResponse {
+        long orderId;
         List<Long> bookIds;
     }
 }
