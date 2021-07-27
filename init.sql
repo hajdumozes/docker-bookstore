c6024227f17ca742b81611483ea41cbd847718dd
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-07-27 17:29:55 +0200

    init tables

--- init.sql
+++ init.sql
@@ -1,3 +1,5 @@
+-- Initialize table BOOK
+
 CREATE TABLE BOOK
 (
     id    serial PRIMARY KEY,
@@ -6,3 +8,26 @@ CREATE TABLE BOOK
 
 INSERT INTO BOOK(title)
 VALUES ('The Little Prince');
+
+INSERT INTO BOOK(title)
+VALUES ('Harry Potter and the Philosopher''s Stone');
+
+INSERT INTO BOOK(title)
+VALUES ('Odyssey');
+
+INSERT INTO BOOK(title)
+VALUES ('Clean Code');
+
+-- Initialize table Customer
+
+CREATE TABLE CUSTOMER
+(
+    id   serial PRIMARY KEY,
+    name VARCHAR(200) UNIQUE NOT NULL
+);
+
+INSERT INTO CUSTOMER(name)
+VALUES ('John Doe');
+
+INSERT INTO CUSTOMER(name)
+VALUES ('Jane Doe');
