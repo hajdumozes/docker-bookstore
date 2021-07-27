fec7cc68565e09a9f49fa2f0f6b0c17bda978ccc
Mózes Hajdú <Mozes.Hajdu@ibm.com>
2021-07-27 21:09:42 +0200

    repository annotation

--- src/main/java/com/demo/docker/repository/BookRepository.java
+++ src/main/java/com/demo/docker/repository/BookRepository.java
@@ -2,6 +2,8 @@ package com.demo.docker.repository;
 
 import com.demo.docker.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
--- src/main/java/com/demo/docker/repository/CustomerOrderRepository.java
+++ src/main/java/com/demo/docker/repository/CustomerOrderRepository.java
@@ -2,6 +2,12 @@ package com.demo.docker.repository;
 
 import com.demo.docker.entity.CustomerOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface CustomerOrderRepository extends JpaRepository<CustomerOrder, Long> {
+
+    List<CustomerOrder> findAllByCustomerId(long customerId);
 }
